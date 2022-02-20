733ec2535a026bbc03069966e416dc9a4d3fbb30
Hothomir <jaskula242@gmail.com>
2022-02-19 22:42:58 -0500

    Included Inky wHAT dependencies.

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-# Weather Report
-    # Raspberry Pi Project, featuring a Inky wHAT e-ink display.
-    # The Weather Report is meant to display the weather in a more visually pleasing layout,
-    # which will combine assets such as hourly readings, date and time, and whatever comes along.
-    
-    # The idea is to create a UI that resembles something like a dashboard that you'd see on TV or in a car.
-
 #TO DO:
 # - API key from Weather Underground to get weather info.
 # - Output current temperature of home location onto Inky wHAT.
 
+#Inky Libraries
+from inky import InkyWHAT
+from PIL import Image, ImageFont, ImageDraw
+
+# pyOWM Libraries
 from pyowm import OWM
 from pyowm.utils import config
 from pyowm.utils import timestamps
@@ -20,4 +18,18 @@ one_call = mgr.one_call(lat=39.77317, lon=-75.051277)
 # Here put your city and Country ISO 3166 country codes
 one_call.forecast_daily[0].temperature('fahrenheit').get('feels_like_morn', None)
 
-print(one_call.forecast_daily)
+img = Image.new("P", (inky_display.WIDTH, inky.display.HEIGHT))
+draw = ImageDraw.Draw(img)
+
+font = ImageFont.truetype(FredokaOne, 24)
+
+message = "Current Temp: 26 F"
+w, h = font.getsize(message)
+x = (inky,display.WIDTH / 2) - (w / 2)
+y = (inky,display.HEIGHT / 2) - (w / 2)
+
+#print(one_call.forecast_daily)
+
+draw.text((x, y), message, inky_display.YELLOW, font)
+inky_display.set_image(img)
+inky_display.show()
