580c1d88b872b0f217f358632cf10ae5a42d28f4
Hothomir <jaskula242@gmail.com>
2022-02-20 16:27:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,13 @@
 #Inky Libraries
 from inky import InkyWHAT
 from PIL import Image, ImageFont, ImageDraw
-from font_fredoka_one import FredokaOne
 
 # pyOWM Libraries
 from pyowm import OWM
 from pyowm.utils import config
 from pyowm.utils import timestamps
+
+PIXEL_FONT = RESOURCES + "fonts/Pixel12x10.ttf"
 
 owm = OWM("c7f275b2d16f8329784620d02222e9ee")
 mgr = owm.weather_manager()
@@ -25,7 +26,7 @@ inky_display.set_border(inky_display.WHITE)
 img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
 draw = ImageDraw.Draw(img)
 
-font = ImageFont.truetype(FredokaOne, 24)
+font = ImageFont.truetype(PIXEL_FONT, 24)
 
 message = "Current Temp: 26 F"
 w, h = font.getsize(message)
