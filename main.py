9f19ab21a0aedc0f5c0cdf0e796eac70734e0d63
hothomir <jaskula242@gmail.com>
2022-02-20 21:52:39 -0500

    Initialization of GUI layout.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #TO DO: - API key from Weather Underground to get weather info. - Output current temperature of home location onto Inky wHAT.
 
 import os, sys
+import datetime
 
 #Inky Libraries
 from inky import InkyWHAT
@@ -14,7 +15,10 @@ from pyowm.utils import timestamps
 CURR_DIR = os.path.dirname(os.path.realpath(__file__)) + "/"
 RESOURCES = CURR_DIR + "resources/"
 PIXEL_FONT = RESOURCES + "fonts/Pixel12x10.ttf"
+Terminal_FONT = RESOURCES + "fonts/terminal-grotesque.ttf"
+TimeDate = datetime.datetime.now()
 
+#OpenWeatherMap Integration
 owm = OWM("c7f275b2d16f8329784620d02222e9ee")
 mgr = owm.weather_manager()
 weather = mgr.weather_at_place("Turnersville,US").weather
@@ -29,15 +33,17 @@ img = Image.open("/home/pi/weather-report/resources/background/test.png")
 draw = ImageDraw.Draw(img)
 
 font = ImageFont.truetype(PIXEL_FONT, 18)
+font2 = ImageFont.truetype(Terminal_FONT, 14)
 
-message = "Current REAL Temp: "+str(avgTemp)+"F"
-w, h = font.getsize(message)
+currentTemp = "Current REAL Temp: "+str(avgTemp)+"F"
+w, h = font.getsize(currentTemp)
 #x = (inky_display.WIDTH / 2) - (w / 2)
 #y = (inky_display.HEIGHT / 2) - (w / 2)
 
-print("Current REAL Temp: "+str(avgTemp)+"F")
+draw.text((5, 3),TimeDate.strftime("%m-%d-%Y"), inky_display.WHITE, font=font2) 
+draw.text((350, 3),TimeDate.strftime("%H:%M"), inky_display.WHITE, font=font2)
 
-draw.text((20, 60), message, inky_display.YELLOW, font)
+draw.text((20, 60), currentTemp, inky_display.YELLOW, font)
 
 inky_display.set_image(img)
 inky_display.show()
