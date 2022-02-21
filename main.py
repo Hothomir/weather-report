fc6daefc9072abdb8f64e0144b72e9b05fd013a7
hothomir <jaskula242@gmail.com>
2022-02-21 02:06:29 +0000

    Created resources folder. Main.py outputs current temp.

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-#TO DO:
-# - API key from Weather Underground to get weather info.
-# - Output current temperature of home location onto Inky wHAT.
+#TO DO: - API key from Weather Underground to get weather info. - Output current temperature of home location onto Inky wHAT.
 
 import os, sys
 
@@ -21,23 +19,25 @@ owm = OWM("c7f275b2d16f8329784620d02222e9ee")
 mgr = owm.weather_manager()
 weather = mgr.weather_at_place("Turnersville,US").weather
 
-temp_dict_fahrenheit = weather.temperature('fahrenheit')
+getTemp = weather.temperature('fahrenheit')
+avgTemp =int(getTemp["temp"])
 
 inky_display = InkyWHAT("yellow")
 inky_display.set_border(inky_display.WHITE)
 
-img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
+img = Image.open("/home/pi/weather-report/resources/background/test.png")
 draw = ImageDraw.Draw(img)
 
-font = ImageFont.truetype(PIXEL_FONT, 24)
+font = ImageFont.truetype(PIXEL_FONT, 18)
 
-message = "Current Temp: 26 F"
+message = "Current REAL Temp: "+str(avgTemp)+"F"
 w, h = font.getsize(message)
-x = (inky_display.WIDTH / 2) - (w / 2)
-y = (inky_display.HEIGHT / 2) - (w / 2)
+#x = (inky_display.WIDTH / 2) - (w / 2)
+#y = (inky_display.HEIGHT / 2) - (w / 2)
 
-print("Current REAL Temperature: "+temp_dict_fahrenheit+"F")
+print("Current REAL Temp: "+str(avgTemp)+"F")
 
-draw.text((x, y), message, inky_display.YELLOW, font)
+draw.text((20, 60), message, inky_display.YELLOW, font)
+
 inky_display.set_image(img)
 inky_display.show()
