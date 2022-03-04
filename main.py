66cc39031689def83748b39afd83655dd10f9389
hothomir <jaskula242@gmail.com>
2022-03-03 23:42:26 -0500

    Added project name. Improving layout of display.

--- main.py
+++ main.py
@@ -25,20 +25,22 @@ Mister_Pixel_FONT = RESOURCES + "fonts/Mister_Pixel_Regular.otf"
 B_FONT = RESOURCES + "fonts/04B_03.ttf"
 VG5000_FONT = RESOURCES + "fonts/VG5000-Regular.otf"
 
-
 TimeDate = datetime.datetime.now()
-degreeSign = u'\N{DEGREE SIGN}'
+degreeSign = u"\N{DEGREE SIGN}"
 
 #OpenWeatherMap Integration
 owm = OWM("c7f275b2d16f8329784620d02222e9ee")
 mgr = owm.weather_manager()
 weather = mgr.weather_at_place("Turnersville,US").weather
 
-getTemp = weather.temperature('fahrenheit')
-avgTemp = int(getTemp["temp"]) #get current temp
+getTemp = weather.temperature("fahrenheit") #enables temp in fahrenheit
+curTemp = int(getTemp["temp"]) #get current temp
 
 hiTemp = int(getTemp["temp_max"])
 loTemp = int(getTemp["temp_min"])
+
+getWind = weather.wind(unit="miles_hour")
+curWind = int(getWind["speed"])
 
 one_call = mgr.one_call(lat=39.7729, lon=75.0519)
 
@@ -50,33 +52,34 @@ inky_display.set_border(inky_display.WHITE)
 img = Image.open("/home/pi/weather-report/resources/background/weather-report-bg.png")
 draw = ImageDraw.Draw(img)
 
-font = ImageFont.truetype(PIXEL_FONT, 10)
+font = ImageFont.truetype(PIXEL_FONT, 12)
 font_medium = ImageFont.truetype(VG5000_FONT, 17)
 font_big = ImageFont.truetype(VG5000_FONT, 64)
 font2 = ImageFont.truetype(VG5000_FONT, 10)
 
-currentTemp = str(avgTemp)+degreeSign
+currentTemp = str(curTemp)+degreeSign
 
-currentHiTemp = "MAX: "+str(hiTemp)+degreeSign #current temp max
-currentLoTemp ="MIN: "+str(loTemp)+degreeSign #current temp min
+currentHiTemp = "H: "+str(hiTemp)+degreeSign #current temp max
+currentLoTemp ="L: "+str(loTemp)+degreeSign #current temp min
 
 currentHumidity = "HUMIDITY: "+str(one_call.current.humidity)+"%"
-currentWind = "WIND: N/A"
+currentWind = "WIND: "+str(curWind)+"MPH"
 
 w, h = font.getsize(currentTemp)
 #x = (inky_display.WIDTH / 2) - (w / 2)
 #y = (inky_display.HEIGHT / 2) - (w / 2)
 
 #draw data and text onto display
-draw.text((5, 3),TimeDate.strftime("%m-%d-%Y"), inky_display.WHITE, font2) #Time
-draw.text((370, 3),TimeDate.strftime("%H:%M"), inky_display.WHITE, font2) #Date
+draw.text((5, 4),TimeDate.strftime("%m-%d-%Y"), inky_display.WHITE, font2)	#Time
+draw.text((368, 4),TimeDate.strftime("%H:%M"), inky_display.WHITE, font2)	#Date
+draw.text((160, 4), "WEATHER REPORT", inky_display.WHITE, font2)		#project name
 
-draw.text((20, 60), currentTemp, inky_display.BLACK, font_big)
-draw.text((20, 130), currentHiTemp, inky_display.BLACK, font)
-draw.text((20, 140), currentLoTemp, inky_display.BLACK, font)
+draw.text((140, 60), currentTemp, inky_display.BLACK, font_big)
+draw.text((140, 130), currentHiTemp, inky_display.BLACK, font)
+draw.text((140, 140), currentLoTemp, inky_display.BLACK, font)
  
-draw.text((200, 100), currentHumidity, inky_display.BLACK, font_medium)
-draw.text((200,120), currentWind, inky_display.BLACK, font_medium)
+draw.text((280, 100), currentHumidity, inky_display.BLACK, font_medium)
+draw.text((280,120), currentWind, inky_display.BLACK, font_medium)
 
 
 inky_display.set_image(img)
