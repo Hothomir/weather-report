7adac43bd1dd575f5f35bf7d7f0336eca0ab329a
hothomir <jaskula242@gmail.com>
2022-03-03 23:13:10 -0500

    Added temperature min and max. Updated background.

--- main.py
+++ main.py
@@ -3,8 +3,6 @@
 # - Display location, humidity, wind, precipitation
 # - Future weather at bottom of display, showing 2 hour periods
 
-# Sketch out how the weather info will look like!
-
 import os, sys
 import datetime
 
@@ -37,7 +35,10 @@ mgr = owm.weather_manager()
 weather = mgr.weather_at_place("Turnersville,US").weather
 
 getTemp = weather.temperature('fahrenheit')
-avgTemp =int(getTemp["temp"]) #get current temp
+avgTemp = int(getTemp["temp"]) #get current temp
+
+hiTemp = int(getTemp["temp_max"])
+loTemp = int(getTemp["temp_min"])
 
 one_call = mgr.one_call(lat=39.7729, lon=75.0519)
 
@@ -46,16 +47,21 @@ one_call.current.humidity #get current humidity
 inky_display = InkyWHAT("yellow")
 inky_display.set_border(inky_display.WHITE)
 
-img = Image.open("/home/pi/weather-report/resources/background/test.png")
+img = Image.open("/home/pi/weather-report/resources/background/weather-report-bg.png")
 draw = ImageDraw.Draw(img)
 
 font = ImageFont.truetype(PIXEL_FONT, 10)
-font_medium = ImageFont.truetype(VG5000_FONT, 25)
+font_medium = ImageFont.truetype(VG5000_FONT, 17)
 font_big = ImageFont.truetype(VG5000_FONT, 64)
 font2 = ImageFont.truetype(VG5000_FONT, 10)
 
 currentTemp = str(avgTemp)+degreeSign
+
+currentHiTemp = "MAX: "+str(hiTemp)+degreeSign #current temp max
+currentLoTemp ="MIN: "+str(loTemp)+degreeSign #current temp min
+
 currentHumidity = "HUMIDITY: "+str(one_call.current.humidity)+"%"
+currentWind = "WIND: N/A"
 
 w, h = font.getsize(currentTemp)
 #x = (inky_display.WIDTH / 2) - (w / 2)
@@ -66,8 +72,12 @@ draw.text((5, 3),TimeDate.strftime("%m-%d-%Y"), inky_display.WHITE, font2) #Time
 draw.text((370, 3),TimeDate.strftime("%H:%M"), inky_display.WHITE, font2) #Date
 
 draw.text((20, 60), currentTemp, inky_display.BLACK, font_big)
-
+draw.text((20, 130), currentHiTemp, inky_display.BLACK, font)
+draw.text((20, 140), currentLoTemp, inky_display.BLACK, font)
+ 
 draw.text((200, 100), currentHumidity, inky_display.BLACK, font_medium)
+draw.text((200,120), currentWind, inky_display.BLACK, font_medium)
+
 
 inky_display.set_image(img)
 inky_display.show()
